317cbc2fb567b8ad4a9ceda289fb411bbb110d46
compactcoders <renusrisivakumar100@gmail.com>
2025-04-20 12:01:29 +0530

    vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/ACM-W/',   // 👈 VERY IMPORTANT for Vercel sub-path
   plugins: [react()],
-  base: '/ACM-W/', // Ensure Vite knows the app is served under /ACM-W/
-});
+})
